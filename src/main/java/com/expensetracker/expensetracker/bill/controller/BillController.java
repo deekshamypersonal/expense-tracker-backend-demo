5c95aa21c263298f6c5e7db403d7ecf347d8c2ad
deekshamypersonal <deeksha.my.personal@gmail.com>
2024-12-25 13:52:52 -0800

    Added logging to file upload

--- src/main/java/com/expensetracker/expensetracker/bill/controller/BillController.java
+++ src/main/java/com/expensetracker/expensetracker/bill/controller/BillController.java
@@ -1,19 +1,27 @@
 package com.expensetracker.expensetracker.bill.controller;
 
 import com.expensetracker.expensetracker.bill.service.BillService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class BillController {
+
+    private static final Logger logger = LoggerFactory.getLogger(BillController.class);
 
     @Autowired
     private BillService billService;
 
     @PostMapping("/upload")
     public String handleFileUpload(@RequestParam("file") MultipartFile file) {
+        logger.info("handleFileUpload accessed.");
         return billService.processFile(file);
+
     }
 
 }
